675c225ed8fd6387ad6e8b7a050d5536795f5350
ozcan.ali94 <ozcan.ali94@gmail.com>
2016-05-11 06:34:39 -0400

    Correction 14/20, MAXIME FAIVRE

--- films.php
+++ films.php
@@ -1,12 +1,13 @@
 <html>
   <head>
-    <title>Films</title>
+    <title>Films</title> 
   </head>
   <body>
     <form method='post' action='films.php'>
-      <select name='realisateur' value='Hitchcock'>
+      <select name='realisateur' value='Hitchcock'> 
         <?php
-        $link =mysqli_connect("sql8.freemysqlhosting.net","sql8117996","xMd12ZdBgn","sql8117996");
+       // $link =mysqli_connect("sql8.freemysqlhosting.net","sql8117996","xMd12ZdBgn","sql8117996"); // Probleme de connection
+         $link = mysqli_connect("dwarves.iut-fbleau.fr","carlu","ludo1811","carlu"); // pour pouvoir corriger je change de base de données
         if(!$link) die("pb");
         $resultat=mysqli_query($link,"SELECT Distinct nom From Film Join Artiste On (idMes=idArtiste)");
         if($resultat){
@@ -39,10 +40,10 @@
       if(isset($_GET["nom"])){
         $nom = $_GET['nom'];
       
-        $link = mysqli_connect("sql8.freemysqlhosting.net","sql8117996","xMd12ZdBgn","sql8117996");
-        if(!$link) die ("pb");
+       // $link = mysqli_connect("sql8.freemysqlhosting.net","sql8117996","xMd12ZdBgn","sql8117996"); // Il y a une erreur de connection
+        $link = mysqli_connect("dwarves.iut-fbleau.fr","carlu","ludo1811","carlu"); // test avec une autre database
         $resultat=mysqli_query($link,"SELECT titre,annee,genre, nom FROM Film Join Artiste On (idMes =idArtiste)");
-    
+    // Pas de sécurité pour la se connecter ( oublie du DIE ) ==> Réeutiliser la syntaxe écris plus-haut.
          if($resultat){
       
           foreach($resultat as $film){
@@ -56,9 +57,15 @@
             }
           }
         }
-      }
+       }
     else
-      die('Erreur de connexion (' . mysqli_connect_errno() . ') '. mysqli_connect_error());
+      die('Erreur de connexion (' . mysqli_connect_errno() . ') '. mysqli_connect_error()); // Code propre , problème de connection , affichage de la liste déroulante OK, méthode POST OK
+      // Bonne utilisation du FOREACH, cependant pas de choix pour TOUT les réalisateur
+      // NOTE EXO 1: 7
+       //NOTE EXO 2: 6 ( realisation de la liste déroulante + méthode post )
+      // NOTE EXO 3 : 0 
+      // Note artistique : 1
+       // NOTE TOTAL : 14/20 ( correcteur : MAXIME FAIVRE)
     ?>
     </table>
   </body>
